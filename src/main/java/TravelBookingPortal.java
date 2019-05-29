73d4bb600a2efaafc452b221ad7ad11713a789ac
takato.horikoshi <takato.h0rikosh1@gmail.com>
2019-05-29 11:44:55 +0900

    wip

--- src/main/java/TravelBookingPortal.java
+++ src/main/java/TravelBookingPortal.java
@@ -17,47 +17,47 @@ abstract class TravelCompany {
 class TravelInfo {
 }
 
-public class TravelBookingPortal {
-    ExecutorService exec;
-
-    private class QuoteTask implements Callback<TravelQuote> {
-        TravelCompany company;
-        TravelInfo travelInfo;
-
-        QuoteTask(TravelCompany company, TravelInfo travelInfo) {
-        }
-
-        public TravelQuote call() throws Exception {
-            return company.solicitQuote(travelInfo);
-        }
-
-    }
-
-    public List<TravelQuote> getRankedTravelQuotes(
-            TravelInfo travelInfo, Set<TravelCompany> companies,
-            Comparator<TravelQuote> ranking, long time, TimeUnit unit)
-            throws InterruptedException {
-        List<QuoteTask> tasks = new ArrayList<QuoteTask>();
-        for (TravelCompany company : companies)
-            tasks.add(new QuoteTask(company, travelInfo));
-
-        List<Future<TravelQuote>> futures =
-                exec.invokeAll(tasks, time, unit);
-
-        List<TravelQuote> quotes =
-                new ArrayList<TravelQuote>(tasks.size());
-        Iterator<QuoteTask> taskIterator = tasks.iterator();
-        for (Future<TravelQuote> f : futures) {
-            QuoteTask task = taskIterator.next();
-            try {
-                quotes.add(f.get());
-            } catch (ExecutionException e) {
-                quotes.add(task.getFailureQuote(e.getCause()));
-            } catch (CancellationException e) {
-                quotes.add(task.getTimeoutQuote(e));
-            }
-        }
-        Collections.sort(quotes, ranking);
-        return quotes;
-    }
-}
+//public class TravelBookingPortal {
+//    ExecutorService exec;
+//
+//    private class QuoteTask implements Callback<TravelQuote> {
+//        TravelCompany company;
+//        TravelInfo travelInfo;
+//
+//        QuoteTask(TravelCompany company, TravelInfo travelInfo) {
+//        }
+//
+//        public TravelQuote call() throws Exception {
+//            return company.solicitQuote(travelInfo);
+//        }
+//
+//    }
+//
+//    public List<TravelQuote> getRankedTravelQuotes(
+//            TravelInfo travelInfo, Set<TravelCompany> companies,
+//            Comparator<TravelQuote> ranking, long time, TimeUnit unit)
+//            throws InterruptedException {
+//        List<QuoteTask> tasks = new ArrayList<QuoteTask>();
+//        for (TravelCompany company : companies)
+//            tasks.add(new QuoteTask(company, travelInfo));
+//
+//        List<Future<TravelQuote>> futures =
+//                exec.invokeAll(tasks, time, unit);
+//
+//        List<TravelQuote> quotes =
+//                new ArrayList<TravelQuote>(tasks.size());
+//        Iterator<QuoteTask> taskIterator = tasks.iterator();
+//        for (Future<TravelQuote> f : futures) {
+//            QuoteTask task = taskIterator.next();
+//            try {
+//                quotes.add(f.get());
+//            } catch (ExecutionException e) {
+//                quotes.add(task.getFailureQuote(e.getCause()));
+//            } catch (CancellationException e) {
+//                quotes.add(task.getTimeoutQuote(e));
+//            }
+//        }
+//        Collections.sort(quotes, ranking);
+//        return quotes;
+//    }
+//}
--- src/main/scala/example/TaskExecutionWebServer.scala
+++ src/main/scala/example/TaskExecutionWebServer.scala
@@ -1,5 +1,49 @@
 package example
 
-class TaskExecutionWebServer {
+import java.net.ServerSocket
+import java.util.concurrent.{Executor, Executors}
+
+object TaskExecutionWebServer {
+
+  private final lazy val NThreads: Int = 100
+  private final lazy val exec: Executor = Executors.newFixedThreadPool(NThreads)
+
+  def main(args: Array[String]): Unit = {
+    val socket = new ServerSocket(80)
+    while (true) {
+      socket.accept()
+      val task = new Runnable {
+        override def run(): Unit = {
+          println("Hello World!!!")
+        }
+      }
+      exec.execute(task)
+    }
+  }
+
+}
+
+object SingleThreadWebServer {
+  def main(args: Array[String]): Unit = {
+    val socket = new ServerSocket(80)
+    while (true) {
+      socket.accept()
+      println("Hello SingleThreadWebServer!!!")
+    }
+  }
+}
+
+object ThreadPerTaskWebServer {
+  def main(args: Array[String]): Unit = {
+    val socket = new ServerSocket(80)
+    while (true) {
+      socket.accept()
+      val task = new Runnable {
+        override def run(): Unit =
+          println("Hello ThreadPerTaskWebServer!!!")
+      }
+      new Thread(task).start()
+    }
+  }
 
 }
